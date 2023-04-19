92e9f0dbcdea7f5039b74fdfa651aa9b7c7e8fd2
Gorgomel <brunnosink2@gmail.com>
2023-04-19 16:13:12 -0300

    Implementação otimizada

--- Programacao_Dinamica/problema_do_troco.c
+++ Programacao_Dinamica/problema_do_troco.c
@@ -1,47 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*A função troco_moedas recebe como parâmetros um vetor moedas com o conjunto de moedas, um inteiro n com a quantidade de 
-moedas, e um inteiro troco com o valor do troco a ser dado. 
-A função retorna um inteiro que representa a quantidade de formas possíveis para devolver o troco utilizando as moedas 
-do conjunto.
-
-O algoritmo utiliza uma matriz mat para armazenar a solução parcial do problema. A matriz tem troco + 1 linhas e n + 1 
-colunas, onde a linha i representa o valor do troco e a coluna j representa o conjunto de moedas utilizado.
-
-A primeira coluna da matriz é inicializada com zero, pois não é possível dar um troco com valor zero utilizando moedas 
-de qualquer conjunto. A primeira linha da matriz é inicializada com um, pois é possível dar um troco com qualquer valor 
-utilizando apenas uma moeda de valor igual ao valor do troco.
-
-O preenchimento da matriz é feito utilizando programação dinâmica. Para cada célula (i, j) da matriz, o algoritmo verifica
-se a moeda moedas[j - 1] pode ser utilizada para dar um troco de valor i. Se for possível, a solução da subestrutura 
-anterior mat[i - moedas[j - 1]][j] é somada à solução da subestrutura anterior mat[i][j - 1].
-
-Ao final do preenchimento da matriz, a solução do problema é dada pela célula mat[troco][n], que representa a quantidade de 
-formas possíveis para dar um troco de valor troco utilizando o conjunto de moedas moedas.*/
-
 int troco_moedas(int *moedas, int troco, int n){
 
     int mat[troco + 1][n + 1];
-    int i, j;
-
-
-    for(i = 0 ; i <= troco ; i++)
-        mat[i][0] = 0;
+    int i, j, x, y;
 
     for(i = 0 ; i <= n ; i++)
-        mat[0][i] = 1;
+        mat[0][i] = moedas[0];
 
     for(i = 1 ; i <= troco ; i++){
-        for(j = 1 ; j <= n ; j++){
-            mat[i][j] = mat[i][j - 1];
-
-            if(moedas[j - 1] <= i)
-                mat[i][j] += mat[i - moedas[j - 1]][j];
+        for(j = 0 ; j < n ; j++){
+            x = ((i - moedas[j]) >= 0) ? mat[i - moedas[j]][j] : 0;
+            y = (j >= 1) ? mat[i][j - 1] : 0;
+            mat[i][j] = x + y;
         }
     }
 
-    return mat[troco][n];
+    return mat[troco][n - 1];
 }
    
 
